a8db6fc1a67efbf4e12c4317bd019c6b69771bfd
Lucas Videla <videla.lucas@gmail.com>
2023-04-02 20:22:08 -0300

    Removed things from old PA

--- complejidad-computacional-street-numbers/src/edu/unlam/progava/complejidad/estrategias/StreetNumbersStrategy.java
+++
@@ -1,7 +0,0 @@
-package edu.unlam.progava.complejidad.estrategias;
-
-public interface StreetNumbersStrategy {
-
-	public int alturaDeLaCasa(int ultimoNumero);
-
-}
--- graphs-base/main/graph/coloring/order/OrderMethod.java
+++
@@ -1,9 +0,0 @@
-package graph.coloring.order;
-
-import graph.Graph;
-
-public interface OrderMethod {
-	
-	public Order generateOrder(Graph graph);
-
-}
