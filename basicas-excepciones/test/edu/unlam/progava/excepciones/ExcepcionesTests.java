106dc66a26b546ee4b347168860954b62268bbac
Federico Gasior <federicogasior@hotmail.com>
2024-09-18 14:50:11 -0300

    Update Junit examples in basicas-excepciones

--- basicas-excepciones/test/edu/unlam/progava/excepciones/ExcepcionesTests.java
+++ basicas-excepciones/test/edu/unlam/progava/excepciones/ExcepcionesTests.java
@@ -1,51 +1,54 @@
 package edu.unlam.progava.excepciones;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import org.junit.Assert;
-import org.junit.Test;
 
 public class ExcepcionesTests {
 
-	@Test(expected = ArithmeticException.class)
+	@Test
 	public void lanzamientoDeExcepcion() {
-		int divisionPorCero = 5 / 0;
-		System.out.println(divisionPorCero);
-		Assert.fail("No debería llegar a este punto");
+		assertThrows(ArithmeticException.class, () -> {
+			int divisionPorCero = 5 / 0;
+		});
 	}
 
 	@Test
 	public void atrapamosLaExcepcion() {
 		try {
 			int divisionPorCero = 5 / 0;
-			System.out.println(divisionPorCero);
-			Assert.fail("No debería llegar a este punto");
-		} catch (ArithmeticException excepcion) {
-			excepcion.printStackTrace();
+
+			fail("No debería llegar a este punto");
+		} catch (ArithmeticException exception) {
+			exception.printStackTrace();
 		}
+		System.out.println("Sigue ejecutandose");
 	}
 
 	@Test
 	public void manejadorDeArchivos() {
 		File file = new File(""); // archivo inexistente
-		Scanner input = null;
-		try {
-			input = new Scanner(file);
-			Assert.fail("No debería llegar a este punto");
-		} catch (FileNotFoundException excepcion) {
-			excepcion.printStackTrace();
-		} finally {
+		// try-with-resource
+		try (Scanner input = new Scanner(file)) {
+			fail("No debería llegar a este punto");
+		} catch (FileNotFoundException exception) {
+			exception.printStackTrace();
+		} /*finally {
 			if (input != null) {
 				input.close();
 			}
-		}
+		}*/
+
 	}
 
 	@Test
 	public void excepcionPropia() {
-		
+		// Inner class
 		class MiExcepcion extends Exception {
 			private static final long serialVersionUID = -495059613251522648L;
 
@@ -54,17 +57,14 @@ public class ExcepcionesTests {
 			}
 		}
 
-		try {
+		assertThrows(MiExcepcion.class, () -> {
 			throw new MiExcepcion("Ejemplo de explicación de error");
-			// Assert.fail();
-		} catch (MiExcepcion excepcion) {
-			excepcion.printStackTrace();
-		}
+		});
 	}
 
 	@Test
 	public void excepcionPropiaEnvolviendoExcepcionOriginal() {
-		
+		// Inner class
 		class MiExcepcion extends Exception {
 			private static final long serialVersionUID = -6146170257956329871L;
 
@@ -73,17 +73,15 @@ public class ExcepcionesTests {
 			}
 		}
 
-		try {
+		assertThrows(MiExcepcion.class, () -> {
 			try {
 				int divisionPorCero = 5 / 0;
-				System.out.println(divisionPorCero);
-				Assert.fail("No debería llegar a este punto");
+
+				fail("No debería llegar a este punto");
 			} catch (ArithmeticException excepcion) {
+				// Envoltura
 				throw new MiExcepcion(excepcion);
-				// Assert.fail();
 			}
-		} catch (MiExcepcion excepcion) {
-			excepcion.printStackTrace();
-		}
+		});
 	}
 }
